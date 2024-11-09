380c3e7c5e0f39011a5a8f9a688a09de4b3823d2
Filobateer Essam <filobateer.essam@sherkety.net>
2024-11-09 12:47:10 +0200

    Finished Update & Delete Operation in Array

--- main.py
+++ main.py
@@ -28,6 +28,16 @@ my_posts = [
         "title": "Third Post",
         "content": "This is the third post",
         "id" : 3
+    },
+    {
+        "title": "Fourth Post",
+        "content": "This is the fourth post",
+        "id" : 4
+    },
+    {
+        "title": "Fifth Post",
+        "content": "This is the fifth post",
+        "id" : 5
     }
 ]
 
@@ -35,6 +45,12 @@ def findpost(id):
     for p in my_posts:
         if p["id"] == id:
             return p
+
+def findIndexArr(id):
+    for i, post in enumerate(my_posts):
+        if post["id"] == id:
+            return i
+    
 
 @app.get("/") # called decorator which is used to define the path of the API
 def root():
@@ -49,8 +65,13 @@ def get_posts():
 # Get the Latest Post
 @app.get("/posts/latest")
 def get_latest_post():
-    l_p = my_posts[len(my_posts) - 1]
-    return {"latest_post" :l_p }
+    lst_posts = []
+    for i in range(len(my_posts) - 1, 1, -1):
+        lst_posts.append(my_posts[i])
+    print(lst_posts)
+    return {"latest Posts" : lst_posts}
+    # l_p = my_posts[len(my_posts) - 1]
+    # return {"latest_post" :l_p }
 
 # Get ID of Post
 @app.get("/posts/{id}")
@@ -59,7 +80,7 @@ def get_postID(id : int ):
     if not post :
         raise HTTPException(status_code= status.HTTP_404_NOT_FOUND, detail= f"Post not found for id {id}")
     print(post)
-    return {"post": f"the post {post}"}
+    return {"post": post}
 
 
 
@@ -72,3 +93,34 @@ def create_post(new_post:Post):
     my_posts.append(post_dict)  # Add new post to the list of posts
     return {"data": post_dict}
 
+@app.delete("/posts/{id}")
+def delete_post(id : int):
+    post = findpost(id)
+    post_index = findIndexArr(id)
+    print(post_index)
+    if post not in my_posts:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post not found for id {id}")
+    my_posts.remove(post)
+    return {"detail": f"Post index in Array is {post_index} with id {id}  has been deleted."}
+
+@app.put("/posts/{id}")
+def update_post(id : int , post: Post):
+    
+    post_index = findIndexArr(id)
+    print(post_index) # the place in the Array not the actual id
+    
+    post_withID = findpost(id)
+    
+    if post_withID not in my_posts:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Post not found for id {id}")
+    
+    else:
+        # must convert th e Object into Dic first 
+        post_dict = post.model_dump()
+        post_dict["id"] = id  # replace id with the actual id
+        
+        # update the post in the list
+        my_posts[post_index] = post_dict
+        return {"data": post_dict}
+        
+        
